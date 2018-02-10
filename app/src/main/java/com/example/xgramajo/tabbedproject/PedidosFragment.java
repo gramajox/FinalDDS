f170aa7db0f613a83d27c980fd9b580bd38ae43b
gramajox <xaviergramajo@gmail.com>
2018-02-09 22:22:20 -0300

    Initial commit

---
+++ app/src/main/java/com/example/xgramajo/tabbedproject/PedidosFragment.java
@@ -0,0 +1,25 @@
+package com.example.xgramajo.tabbedproject;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class PedidosFragment extends Fragment {
+    private static final String TAG = "Tab3Fragment";
+
+    private Button btnTEST;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.pedidos_tab,container,false);
+
+
+        return view;
+    }
+}
